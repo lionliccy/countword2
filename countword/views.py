a422bb20bd438018e3f1078c5a8b247bf5e76eff
lionliccy <swlymoure@outlook.com>
2019-06-17 16:03:05 +0800

    词频统计功能

--- countword/views.py
+++ countword/views.py
@@ -7,3 +7,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def count(request):
+    text=request.GET['text']
+    print(request.GET['text'])
+    #词频统计
+    result={}
+    for i in text:
+        if i not in result:
+            result[i]=1
+        else:
+            result[i]+=1
+    print(result)
+    result=sorted(result.items(),key=lambda x:x[1],reverse=True)
+
+    #返回到另一个页面
+    return render(request,'count.html',{'count_result':result})
